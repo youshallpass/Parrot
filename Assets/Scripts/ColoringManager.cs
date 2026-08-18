a976768e7f4badb199d852b8de10cd9144e901e7
JuliusKristensen <juliuskristensen@gmail.com>
2026-08-18 14:15:09 +0200

    Slider for brushsize

--- Assets/Scripts/ColoringManager.cs
+++ Assets/Scripts/ColoringManager.cs
@@ -2,10 +2,10 @@ using UnityEngine;
 
 public class ColoringManager : MonoBehaviour
 {
-    [SerializeField] private float brushSize = 20f;
     [SerializeField] private Color color = Color.red;
     [SerializeField] private SpriteRenderer picture;
     [SerializeField] private SpriteRenderer paintLayer;
+    public float brushSize = 20f;
     private Texture2D canvasTexture;
 
     // Start is called once before the first execution of Update after the MonoBehaviour is created
@@ -58,6 +58,11 @@ public class ColoringManager : MonoBehaviour
     private void SetColor(Color newColor)
     {
         color = newColor;
+    }
+
+    public void SetBrushSize(float newBrushSize)
+    {
+        brushSize = newBrushSize;
     }
 
     private void Paint(Vector2 touchPosition)
