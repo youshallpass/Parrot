d39040a9fc87ed0f1cbcd0722595b947387b36d1
JuliusKristensen <juliuskristensen@gmail.com>
2026-08-18 12:07:23 +0200

    Basic painting works
    Still needs to have the image be transparrent so that the lines are always visible

--- Assets/Scripts/ColoringManager.cs
+++ Assets/Scripts/ColoringManager.cs
@@ -2,16 +2,92 @@ using UnityEngine;
 
 public class ColoringManager : MonoBehaviour
 {
-    [SerializeField] float brushSize = 20f;
+    [SerializeField] private float brushSize = 20f;
+    [SerializeField] private Color color = Color.red;
+    [SerializeField] private SpriteRenderer picture;
+    [SerializeField] private SpriteRenderer paintLayer;
+    private Texture2D canvasTexture;
+
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
-        
+        InitializeCanvasTexture();
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        if (Input.touchCount > 0)
+        {
+            Touch touch = Input.GetTouch(0);
+
+            Vector2 touchPosition = touch.position;
+            Paint(touchPosition);
+        }
+
+#if UNITY_EDITOR
+        if (Input.GetMouseButton(0))
+        {
+            Paint(Input.mousePosition);
+        }
+#endif
+    }
+
+    private void InitializeCanvasTexture()
+    {
+        canvasTexture = new Texture2D(picture.sprite.texture.width, picture.sprite.texture.height, TextureFormat.RGBA32, false);
+        canvasTexture.filterMode = FilterMode.Bilinear;
+
+        Sprite paintSprite = Sprite.Create(canvasTexture, new Rect(0, 0, picture.sprite.texture.width, picture.sprite.texture.height), new Vector2(0.5f, 0.5f), picture.sprite.pixelsPerUnit);
+        paintLayer.sprite = paintSprite;
+
+        ClearCanvas();
+    }
+
+    private void ClearCanvas()
+    {
+        Color[] clearColorArray = new Color[canvasTexture.width * canvasTexture.height];
+        for (var i = 0; i < clearColorArray.Length; i++)
+        {
+            clearColorArray[i] = Color.clear;
+        }
+        canvasTexture.SetPixels(clearColorArray);
+        canvasTexture.Apply();
+    }
+
+    private void SetColor(Color newColor)
+    {
+        color = newColor;
+    }
+
+    private void Paint(Vector2 touchPosition)
+    {
+        Vector3 screenPosition = new Vector3(touchPosition.x, touchPosition.y, -Camera.main.transform.position.z);
+        Vector3 worldPosition = Camera.main.ScreenToWorldPoint(screenPosition);
+
+        if (!picture.bounds.Contains(worldPosition))
+        {
+            return;
+        }
+
+        float x = Mathf.InverseLerp(picture.bounds.min.x, picture.bounds.max.x, worldPosition.x);
+        float y = Mathf.InverseLerp(picture.bounds.min.y, picture.bounds.max.y, worldPosition.y);
+
+        int pixelX = Mathf.RoundToInt(x * canvasTexture.width);
+        int pixelY = Mathf.RoundToInt(y * canvasTexture.height);
+
+        int size = Mathf.RoundToInt(brushSize);
+
+        for (int i = -size / 2; i < size / 2;  i++)
+        {
+            for (int j = -size / 2; j < size / 2; j++)
+            {
+                if (pixelX + i >= 0 && pixelX + i < canvasTexture.width && pixelY + j >= 0 && pixelY + j < canvasTexture.height)
+                {
+                    canvasTexture.SetPixel(pixelX + i, pixelY + j, color);
+                }
+            }
+        }
+        canvasTexture.Apply();
     }
 }
